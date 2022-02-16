459b30b7ff6df8f03a46c6652641d9affc65e493
Michal@SFB2 <michal.marusan@gmail.com>
2022-02-17 00:03:27 +0100

    commit message

---
+++ BlobTrigger1/__init__.py
@@ -0,0 +1,100 @@
+import logging
+from azure.storage.blob import BlobServiceClient
+import azure.functions as func
+import json
+import time
+from requests import get, post
+import os
+from collections import OrderedDict
+import numpy as np
+import pandas as pd
+
+import azure.functions as func
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+
+def main(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob \n"
+                 f"Name: {myblob.name}\n"
+                 f"Blob Size: {myblob.length} bytes")
+    
+    # This is the call to the Form Recognizer endpoint
+    endpoint = r"https://mma-form-reco1.cognitiveservices.azure.com/"
+    apim_key = "***"
+    model_id = "vyuctovatni-net"
+
+    full_recognizer_output = False
+
+    # post_url = endpoint + "/formrecognizer/v2.1/Layout/analyze"
+    # read the PDF document
+    source = myblob.read()
+    text1=os.path.basename(myblob.name)
+
+    document_analysis_client = DocumentAnalysisClient(
+        endpoint=endpoint, credential=AzureKeyCredential(apim_key)
+    )
+
+    # Make sure your document's type is included in the list of document types the custom model can analyze
+    # poller = document_analysis_client.begin_analyze_document_from_url(model_id, documentUrl)
+    poller = document_analysis_client.begin_analyze_document(model_id, source)
+
+    result = poller.result()
+    detected_kv_pairs = []
+    for idx, document in enumerate(result.documents):
+        print("--------Analyzing document #{}--------".format(idx + 1))
+        print("Document has type {}".format(document.doc_type))
+        print("Document has confidence {}".format(document.confidence))
+        print("Document was analyzed by model with ID {}".format(result.model_id))
+        for name, field in document.fields.items():
+            field_value = field.value if field.value else field.content
+            print("......found field {} of type '{}' with value '{}' and with confidence {}".format(name,field.value_type, field_value, field.confidence))
+            detected_kv_pairs.append([name, field_value, field.confidence])
+
+    # convert detected key-value pairs to Pandas dataframe        
+    document_detected_kv_pairs = pd.DataFrame(detected_kv_pairs, columns =['Key', 'Value','Confidence'])
+
+
+    if (full_recognizer_output):
+        # iterate over tables, lines, and selection marks on each page
+        for page in result.pages:
+            print("\nLines found on page {}".format(page.page_number))
+            for line in page.lines:
+                print("...Line '{}'".format(line.content.encode('utf-8')))
+            for word in page.words:
+                print(
+                    "...Word '{}' has a confidence of {}".format(
+                        word.content.encode('utf-8'), word.confidence
+                    )
+                )
+            for selection_mark in page.selection_marks:
+                print(
+                    "...Selection mark is '{}' and has a confidence of {}".format(
+                        selection_mark.state, selection_mark.confidence
+                    )
+                )
+
+        for i, table in enumerate(result.tables):
+            print("\nTable {} can be found on page:".format(i + 1))
+            for region in table.bounding_regions:
+                print("...{}".format(i + 1, region.page_number))
+            for cell in table.cells:
+                print(
+                    "...Cell[{}][{}] has content '{}'".format(
+                        cell.row_index, cell.column_index, cell.content.encode('utf-8')
+                    )
+                )
+        print("-----------------------------------")
+    
+
+    # print(f"Uploading to output blob storage...")
+    # # This is the connection to the blob storage, with the Azure Python SDK
+    # blob_service_client = BlobServiceClient(account_url="mmaunistorage.core.windows.net", credential="SASTOKEN")
+
+    # # blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName="mmaunistorage";AccountKey=\"***\";EndpointSuffix=core.windows.net")
+    # container_client=blob_service_client.get_container_client("nemji-output")
+
+    # # Here is the upload to the blob storage
+    # tab1_csv=document_detected_kv_pairs.to_csv(header=True,index=False,mode='w')
+    # name1=(os.path.splitext(text1)[0]) +'.csv'
+    # container_client.upload_blob(name=name1,data=tab1_csv)
---
+++ sample_analyze_custom_documents.py
@@ -0,0 +1,70 @@
+"""
+This code sample shows Custom Model operations with the Azure Form Recognizer client library. 
+The async versions of the samples require Python 3.6 or later.
+
+To learn more, please visit the documentation - Quickstart: Form Recognizer Python client library SDKs v3.0
+https://docs.microsoft.com/en-us/azure/applied-ai-services/form-recognizer/quickstarts/try-v3-python-sdk
+"""
+
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+endpoint = "https://mma-form-reco1.cognitiveservices.azure.com/"
+key = "***"
+model_id = "vyuctovatni-net"
+
+# documentUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-layout.pdf"
+documentPath = "2100501128.pdf"
+document_analysis_client = DocumentAnalysisClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key)
+)
+
+
+with open(documentPath, "rb") as fd:
+    document = fd.read()
+
+# Make sure your document's type is included in the list of document types the custom model can analyze
+# poller = document_analysis_client.begin_analyze_document_from_url(model_id, documentUrl)
+poller = document_analysis_client.begin_analyze_document(model_id, document)
+
+result = poller.result()
+
+for idx, document in enumerate(result.documents):
+    print("--------Analyzing document #{}--------".format(idx + 1))
+    print("Document has type {}".format(document.doc_type))
+    print("Document has confidence {}".format(document.confidence))
+    print("Document was analyzed by model with ID {}".format(result.model_id))
+    for name, field in document.fields.items():
+        field_value = field.value if field.value else field.content
+        print("......found field {} of type '{}' with value '{}' and with confidence {}".format(name,field.value_type, field_value, field.confidence))
+
+
+# iterate over tables, lines, and selection marks on each page
+for page in result.pages:
+    print("\nLines found on page {}".format(page.page_number))
+    for line in page.lines:
+        print("...Line '{}'".format(line.content.encode('utf-8')))
+    for word in page.words:
+        print(
+            "...Word '{}' has a confidence of {}".format(
+                word.content.encode('utf-8'), word.confidence
+            )
+        )
+    for selection_mark in page.selection_marks:
+        print(
+            "...Selection mark is '{}' and has a confidence of {}".format(
+                selection_mark.state, selection_mark.confidence
+            )
+        )
+
+for i, table in enumerate(result.tables):
+    print("\nTable {} can be found on page:".format(i + 1))
+    for region in table.bounding_regions:
+        print("...{}".format(i + 1, region.page_number))
+    for cell in table.cells:
+        print(
+            "...Cell[{}][{}] has content '{}'".format(
+                cell.row_index, cell.column_index, cell.content.encode('utf-8')
+            )
+        )
+print("-----------------------------------")

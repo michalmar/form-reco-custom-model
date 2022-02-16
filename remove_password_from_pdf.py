da21debc3b8fd1fa476947830aadbada74b0fadb
Michal@SFB2 <michal.marusan@gmail.com>
2022-02-16 23:55:36 +0100

    init

---
+++ remove_password_from_pdf.py
@@ -0,0 +1,22 @@
+import pikepdf
+
+# pdf_loc = input("PDF location: ")
+# pdf_pass = input("PDF password: ")
+pdf_loc = "test.pdf"
+pdf_pass = "abc1234"
+
+pdf = pikepdf.open(pdf_loc, password=pdf_pass)
+
+print("\nProcessing...\n")
+
+# pdf_save = input("Save file as: ")
+# pdf_loc2 = input("Save location: ")
+
+pdf_save = "test-bez-hesla.pdf"
+pdf_loc2 = "."
+
+
+pdf.save(pdf_loc2 + '\\' + pdf_save)
+
+print("The password successfully removed from the PDF")
+print("\aLocation: " + pdf_loc + '\\' + pdf_save)
